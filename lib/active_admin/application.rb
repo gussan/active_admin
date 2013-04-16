6484681e9fa629e297b661b384f37834bd13ae09
gussan <egussan@gmail.com>
2013-04-16 16:25:58 +0900

    load sass plugins

--- lib/active_admin/application.rb
+++ lib/active_admin/application.rb
@@ -255,6 +255,8 @@ module ActiveAdmin
         ::Sass::Engine::DEFAULT_OPTIONS[:load_paths] <<  File.expand_path("../../../app/assets/stylesheets", __FILE__)
       else
         require 'active_admin/sass/css_loader'
+        require 'sass/plugin'
+        require 'sass/plugin/configuration'
         ::Sass::Plugin.add_template_location(File.expand_path("../../../app/assets/stylesheets", __FILE__))
         ::Sass::Plugin.add_template_location(File.expand_path("../sass", __FILE__))
       end
