199edbdbaf26a7d85f2c61d34edb744a5acaac96
gussan <egussan@gmail.com>
2013-05-07 00:13:32 +0900

    Add strong parameter setting example to resource template

--- lib/generators/active_admin/resource/templates/admin.rb
+++ lib/generators/active_admin/resource/templates/admin.rb
@@ -1,3 +1,13 @@
 ActiveAdmin.register <%= class_name %> do
-  
+
+  #
+  # For StrongParameters
+  # 
+  #   !!PLEASE PERMIT REQUIRED PARAMS ONLY!!
+  #
+  # controller do
+  #   def permitted_params
+  #     params.permit!
+  #   end
+  # end
 end
